5e6302c7af25ea2a0bafa6c55268195d2d627325
Aditya Gayan <adityagayan1303@gmail.com>
2026-01-06 11:13:18 +0000

    Added UI

--- script.js
+++ script.js
@@ -10,46 +10,57 @@ function getComputerChoice(){
         return "scissors";
     }
 }
-function getHumanChoice(){
-    let choice =  prompt("Enter your choice");
-    return choice;
-}
 
+    const rock = document.querySelector(".rock");
+    const paper = document.querySelector(".paper");
+    const scissors = document.querySelector(".scissors");
+    const result = document.querySelector(".result");
+    const score = document.querySelector(".score");
+    const display = document.querySelector(".display");
 
-
-function playGame(){
     let humanScore = 0;
     let computerScore = 0;
     function playRound(humanChoice, computerChoice){
         humanChoice = humanChoice.toLowerCase();
         if(humanChoice === computerChoice){
-            console.log("Round tied!");
+            display.textContent = "Round Tied!";
+            score.textContent = `Human score: ${humanScore}\tComputer score: ${computerScore}`;
         }
         else{
             if((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")){
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                display.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
                 computerScore++;
+                score.textContent = `Human score: ${humanScore}\tComputer score: ${computerScore}`;
+                if(computerScore >= 5){
+                    result.textContent = `YOU LOSE`;
+                    rock.disabled = true;
+                    paper.disabled = true;
+                    scissors.disabled = true;
+                }
             }
             else{
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                display.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
                 humanScore++;
+                score.textContent = `Human score: ${humanScore}\tComputer score: ${computerScore}`;
+                if(humanScore >= 5){
+                    result.textContent = `YOU WIN`;
+                    rock.disabled = true;
+                    paper.disabled = true;
+                    scissors.disabled = true;
+                }
             }
         }
     }
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    if(humanScore > computerScore){
-        console.log("Congralutions! You win the game!");
-    }
-    else if(humanScore < computerScore){
-        console.log("You lose the game! Better luck next time");
-    }
-    else{
-        console.log("Game tied!");
-    }
-}
+    
 
-playGame();
+    rock.addEventListener("click", () => {
+        playRound("rock", getComputerChoice());
+        
+    })
+    paper.addEventListener("click", () => {
+        playRound("paper", getComputerChoice());
+        
+    })
+    scissors.addEventListener("click", () => {
+        playRound("scissors", getComputerChoice());
+    })
